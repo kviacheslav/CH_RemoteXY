6c368230e1a2280bf62c48daf5c857f98ce2df80
kviacheslav <k.viacheslav@rambler.ru>
2017-12-26 00:26:46 +0300

    +gsm

---
+++ project4/CH_RemoteXY.h
@@ -0,0 +1,271 @@
+//подключаем библиотеки
+#if defined(CH_LCD)
+	#include  <LiquidCrystal.h> 
+	
+	#define CH_PIN_LITING	10
+	#define CH_BUTTON_NONE   0 //присваиваем постоянное значение для BUTTON_NONE 
+	#define CH_BUTTON_RIGHT  1 //присваиваем постоянное значение для BUTTON_RIGHT
+	#define CH_BUTTON_UP     2 //присваиваем постоянное значение для BUTTON_UP
+	#define CH_BUTTON_DOWN   3 //присваиваем постоянное значение для BUTTON_DOWN 
+	#define CH_BUTTON_LEFT   4 //присваиваем постоянное значение для BUTTON_LEFT
+	#define CH_BUTTON_SELECT 5 //присваиваем постоянное значение для BUTTON_SELECT
+#endif
+#if defined(CH_DHT)
+	#include  <DHT.h>
+	#define CH_DHT_TIMEOUT	120000 
+#endif
+	#include <SD.h>
+	#define CD_PIN 4
+class CH_CRemoteXY: public CRemoteXY {
+ public:
+ 
+ CH_CRemoteXY(const void * _conf, void * _var, const char * _accessPassword, HardwareSerial * _serial, long _serialSpeed, const char * _wifiSsid, const char * _wifiPassword, const char * _macAddress, const char * _cloudServer, uint16_t _cloudPort, const char * _cloudToken)
+#if defined(REMOTEXY_MODE__ETHERNET_LIB_CLOUD) 
+	: CRemoteXY (_conf, _var, _accessPassword, _macAddress, _cloudServer, _cloudPort, _cloudToken)
+#endif 
+#if defined(REMOTEXY_MODE__ESP8266_HARDSERIAL_CLOUD) 
+ :  CRemoteXY(_conf, _var, _accessPassword, _serial,  _serialSpeed,  _wifiSsid,  _wifiPassword, _cloudServer, _cloudPort, _cloudToken)
+ #endif
+ {
+#if defined(CH_LCD)
+	//Инициализируем дисплей
+	lcd= new CH_LCD ;									//: объект управления LCD
+	//pinMode (CH_PIN_LITING, OUTPUT);					//: пин подсветки LCD
+	//analogWrite(CH_PIN_LITING, 500);					//: подсветка 0.5 VCC
+	lcd->begin(16, 2); 									//: 2 строки по 16 символов            
+	lcd->print("INIT...");
+	button= last_button= CH_BUTTON_NONE;
+#endif
+#if defined(CH_DHT)
+	//Инициализируем термометр
+	dht= new CH_DHT;
+	dhtTimeOut= millis();
+	dht_h=dht_t=0;
+#endif
+	Serial3.begin(115200);
+	sd_init();
+ }
+ 
+#if defined(CH_DHT)  
+	DHT *dht;
+	uint32_t dhtTimeOut;
+	float dht_h; 
+	float dht_t;	
+#endif
+#if defined(CH_LCD)
+	LiquidCrystal *lcd;	
+	uint8_t button;  //вводим числовые значения для кнопок
+	uint8_t last_button;
+#endif
+	File myFile; 
+  // 
+  void ch_handler(){
+	int c;
+	if (Serial3.available()) {
+		//Serial.print("available - ");
+		//Serial.println(Serial.available());
+		c = Serial3.read();
+		// open the file. note that only one file can be open at a time,
+		// so you have to close this one before opening another.
+		
+		// if the file opened okay, write to it:
+		if (myFile) {
+			
+			while (c != -1){
+				myFile.write(c);
+				Serial.write(c);
+				c = Serial3.read();
+			}
+			// close the file:
+			myFile.flush();
+		}	
+	}
+	if (Serial.available()) {
+		c = Serial.read();
+		while (c != -1){			
+			Serial3.write(c);
+			c = Serial.read();
+		}
+	}
+#if defined(CH_DHT) 	
+	// Reading temperature or humidity takes about 250 milliseconds!
+	// Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+	if ((millis() - dhtTimeOut) > CH_DHT_TIMEOUT){
+#if defined(REMOTEXY__DEBUGLOGS)
+		REMOTEXY__DEBUGLOGS.println("Humidity reading.");		
+#endif  
+		float h = dht->readHumidity();
+		// Read temperature as Celsius (the default)
+		float t = dht->readTemperature();
+		// Check if any reads failed and exit early (to try again).	
+		if (!isnan(h)){
+			dht_h=h;		  
+		}
+#if defined(REMOTEXY__DEBUGLOGS)
+		else REMOTEXY__DEBUGLOGS.println("Humidity unavailable.");		
+#endif     
+	
+		if (!isnan(t)){
+			dht_t=t;			
+		}
+		dhtTimeOut= millis();
+	}	
+#endif //defined(CH_DHT) 	
+	display_state(RemoteXY.connect_flag);
+  }
+  // if (getmoduleRunning() == 1) then module connected and working
+  uint8_t getmoduleRunning()
+  {
+    return moduleRunning;
+  }
+  
+	void display_state(uint8_t switch_state)
+	{
+#if defined(CH_LCD)
+    lcd->setCursor(0, 1);
+    lcd->print(switch_state,DEC);
+    lcd->setCursor(1, 1);
+    lcd->print(cloudState, DEC);
+    lcd->setCursor(2, 1);
+    lcd->print(moduleRunning,DEC);
+	lcd->setCursor(4, 1);
+	#if defined(CH_DHT) 
+		lcd->print("H:");
+		lcd->setCursor(6, 1);
+		lcd->print(dht_h);
+		lcd->setCursor(10, 1);
+		lcd->print("T:");
+		lcd->setCursor(12, 1);
+		lcd->print(dht_t);	
+	#endif //defined(CH_DHT) 
+#else //defined(CH_LCD)
+		Serial.print(switch_state,DEC);		
+		Serial.print(cloudState, DEC);		
+		Serial.print(moduleRunning,DEC);
+	#if defined(CH_DHT) 
+		Serial.print(" h");
+		Serial.print(dht_h);
+		Serial.print(" t");
+		Serial.println(dht_t);
+	#endif //defined(CH_DHT) 
+			
+#endif //defined(CH_LCD)
+ 	}
+
+#if defined(CH_LCD)	
+	uint8_t getPressedButton() //инициализация переменной
+	{
+	  int buttonValue = analogRead(0); // считываем значения с аналогового входа
+	  if (buttonValue < 100) { //если при нажатии кнопки значение меньше 100
+		return CH_BUTTON_RIGHT;   // выводим значение BUTTON_RIGHT
+	  }
+	  else if (buttonValue < 150) { //если при нажатии кнопки значение меньше 200
+		return CH_BUTTON_UP; // выводим значение BUTTON_UP
+	  }
+	  else if (buttonValue < 350){ //если при нажатии кнопки значение меньше 400
+		return CH_BUTTON_DOWN; // выводим значение BUTTON_DOWN
+	  }
+	  else if (buttonValue < 600){ //если при нажатии кнопки значение меньше 600
+		return CH_BUTTON_LEFT; // выводим значение BUTTON_LEFT
+	  }
+	  else if (buttonValue < 800){ //если при нажатии кнопки значение меньше 800
+		return CH_BUTTON_SELECT; // выводим значение BUTTON_SELECT
+	  }
+	  return CH_BUTTON_NONE; //иначе, выводим значение BUTTON_NONE
+	}
+#endif //defined(CH_LCD)
+
+void sd_init(){
+	if (myFile) {
+		Serial.print("Already to test.txt...");	
+		return;
+	}
+  Serial.print("Initializing SD card...");
+ // Этот пин обязательно должен быть определен как OUTPUT
+ // pinMode(10, OUTPUT);
+ 
+  // Пытаемся проинициализировать модуль
+  if (!SD.begin(CD_PIN)) {
+    Serial.println("Card failed, or not present");
+    // Если что-то пошло не так, завершаем работу:
+    return;
+  }
+  Serial.println("card initialized.");
+  /*Serial.print("\nInitializing SD card...");
+
+  // we'll use the initialization code from the utility libraries
+  // since we're just testing if the card is working!
+  if (!card.init(SPI_HALF_SPEED, CD_PIN)) {
+    Serial.println("initialization failed. Things to check:");
+    Serial.println("* is a card inserted?");
+    Serial.println("* is your wiring correct?");
+    Serial.println("* did you change the CD_PIN pin to match your shield or module?");
+    return;
+  } else {
+    Serial.println("Wiring is correct and a card is present.");
+  }
+
+  // print the type of card
+  Serial.print("\nCard type: ");
+  switch (card.type()) {
+    case SD_CARD_TYPE_SD1:
+      Serial.println("SD1");
+      break;
+    case SD_CARD_TYPE_SD2:
+      Serial.println("SD2");
+      break;
+    case SD_CARD_TYPE_SDHC:
+      Serial.println("SDHC");
+      break;
+    default:
+      Serial.println("Unknown");
+  }*/
+   // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  myFile = SD.open("test.txt", FILE_WRITE);
+
+  // if the file opened okay, write to it:
+  if (myFile) {
+    Serial.print("Ready to test.txt...");
+	
+ /*   myFile.println("testing 1, 2, 3.");
+    // close the file:
+    myFile.close();
+    Serial.println("done.");*/
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+/*
+  // Now we will try to open the 'volume'/'partition' - it should be FAT16 or FAT32
+  if (!volume.init(card)) {
+    Serial.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
+    return;
+  }
+  // print the type and size of the first FAT-type volume
+  uint32_t volumesize;
+  Serial.print("\nVolume type is FAT");
+  Serial.println(volume.fatType(), DEC);
+  Serial.println();
+
+  volumesize = volume.blocksPerCluster();    // clusters are collections of blocks
+  volumesize *= volume.clusterCount();       // we'll have a lot of clusters
+  volumesize *= 512;                            // SD card blocks are always 512 bytes
+  Serial.print("Volume size (bytes): ");
+  Serial.println(volumesize);
+  Serial.print("Volume size (Kbytes): ");
+  volumesize /= 1024;
+  Serial.println(volumesize);
+  Serial.print("Volume size (Mbytes): ");
+  volumesize /= 1024;
+  Serial.println(volumesize);
+
+
+  Serial.println("\nFiles found on the card (name, date and size in bytes): ");
+  root.openRoot(volume);
+
+  // list all files in the card with date and size
+  root.ls(LS_R | LS_DATE | LS_SIZE);*/
+
+}
+};
---
+++ project4/project4.ino
@@ -0,0 +1,164 @@
+/*
+   -- New project --
+   
+   This source code of graphical user interface 
+   has been generated automatically by RemoteXY editor.
+   To compile this code using RemoteXY library 2.3.3 or later version 
+   download by link http://remotexy.com/en/library/
+   To connect using RemoteXY mobile app by link http://remotexy.com/en/download/                   
+     - for ANDROID 4.1.1 or later version;
+     - for iOS 1.2.1 or later version;
+    
+   This source code is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.    
+*/
+
+//////////////////////////////////////////////
+//        RemoteXY include library          //
+//////////////////////////////////////////////
+
+// определение режима соединения и подключение библиотеки RemoteXY 
+#define REMOTEXY_MODE__ETHERNET_LIB_CLOUD
+#include <Ethernet.h>
+/* Ethernet shield used pins: 10(SS) */
+#include <SPI.h>
+/* SPI interface used pins: 11(MOSI), 12(MISO), 13(SCK) */
+#define REMOTEXY__DEBUGLOGS Serial
+#include <RemoteXY.h>
+
+// настройки соединения 
+#define REMOTEXY_SERIAL Serial
+#define REMOTEXY_SERIAL_SPEED 115200
+#define REMOTEXY_WIFI_SSID "viacheslavk"
+#define REMOTEXY_WIFI_PASSWORD "756235D394"
+#define REMOTEXY_SERVER_PORT 6377
+#define REMOTEXY_ETHERNET_MAC "DE:AD:BE:EF:EF:ED"
+#define REMOTEXY_CLOUD_SERVER "cloud.remotexy.com"
+#define REMOTEXY_CLOUD_PORT 6376
+#define REMOTEXY_CLOUD_TOKEN "73b4e6b3bb1459c32b8b9d93dcca3b3d"
+
+
+// конфигурация интерфейса  
+#pragma pack(push, 1)
+uint8_t RemoteXY_CONF[] =
+  { 255,0,0,42,0,20,0,8,13,0,
+  66,132,37,12,29,23,2,24,67,4,
+  9,46,83,8,2,26,41 };
+  
+// структура определяет все переменные вашего интерфейса управления 
+struct {
+
+    // output variable
+  int8_t level_1; // =0..100 положение уровня 
+  char text_1[41];  // =строка UTF8 оканчивающаяся нулем 
+
+    // other variable
+  uint8_t connect_flag;  // =1 if wire connected, else =0 
+
+} RemoteXY;
+#pragma pack(pop)
+
+/////////////////////////////////////////////
+//           END RemoteXY include          //
+/////////////////////////////////////////////
+#define CH_LCD	 		LiquidCrystal(28, 29, 24, 25, 26, 27)
+#define CH_DHT DHT(22, DHT11)
+#define PIN_SWITCH_1 23
+
+#define REMOTEXY_MODULE_TIMEOUT 60000
+#define REMOTEXY_INET_TIMEOUT 120000
+
+#include "CH_RemoteXY.h"
+
+uint32_t moduleTimeOut;
+uint32_t inetTimeOut;
+
+CH_CRemoteXY *ch;
+//#include <SoftwareSerial.h>
+ 
+//SoftwareSerial SoftSerial(5, 6); // RX, TX
+
+void setup() 
+{
+	
+	
+	pinMode (PIN_SWITCH_1, OUTPUT);
+	digitalWrite(PIN_SWITCH_1, LOW);
+	
+	
+	//SoftSerial.begin(115200);
+	//Serial.println("Begin...");
+	ch = new CH_CRemoteXY (RemoteXY_CONF_PROGMEM, &RemoteXY, REMOTEXY_ACCESS_PASSWORD, &REMOTEXY_SERIAL, REMOTEXY_SERIAL_SPEED, REMOTEXY_WIFI_SSID, REMOTEXY_WIFI_PASSWORD, REMOTEXY_ETHERNET_MAC, REMOTEXY_CLOUD_SERVER, REMOTEXY_CLOUD_PORT, REMOTEXY_CLOUD_TOKEN);
+  // TODO you setup code	
+	inetTimeOut= moduleTimeOut= millis();
+	ch->display_state(RemoteXY.connect_flag);
+	Serial.println(memoryFree());
+}
+
+void loop() 
+{ 
+	
+
+	
+	ch->ch_handler();
+	if (ch->getmoduleRunning()){
+		ch->handler ();		
+	  
+	  // TODO you loop code
+	  // используйте структуру RemoteXY для передачи данных
+		
+		if (ch->getCloudState () == REMOTEXY_CLOUD_STATE_WORKING){
+		  inetTimeOut= millis();
+		}else if ((millis() - inetTimeOut) > REMOTEXY_INET_TIMEOUT){
+			
+			digitalWrite(PIN_SWITCH_1, HIGH);
+			delay(3000); 
+			digitalWrite(PIN_SWITCH_1, LOW);
+			ch->initModule ();
+			inetTimeOut= moduleTimeOut= millis();
+		}
+	}
+	else if ((millis() - moduleTimeOut) > REMOTEXY_MODULE_TIMEOUT){	
+		
+#if defined(REMOTEXY__DEBUGLOGS)
+          REMOTEXY__DEBUGLOGS.println();
+          REMOTEXY__DEBUGLOGS.println("Retry initModule...");
+
+#endif
+		ch->display_state(RemoteXY.connect_flag);
+		
+		digitalWrite(PIN_SWITCH_1, HIGH);
+		delay(3000); 
+		digitalWrite(PIN_SWITCH_1, LOW);
+		ch->sd_init();
+		//ch->initModule ();
+		Serial.println(memoryFree());
+		moduleTimeOut= millis();  
+	}
+	
+}
+
+// Переменные, создаваемые процессом сборки,
+// когда компилируется скетч
+extern int __bss_end;
+extern void *__brkval;
+ 
+// Функция, возвращающая количество свободного ОЗУ (RAM)
+int memoryFree()
+{
+   int freeValue;
+   if((int)__brkval == 0)
+      freeValue = ((int)&freeValue) - ((int)&__bss_end);
+   else
+      freeValue = ((int)&freeValue) - ((int)__brkval);
+   return freeValue;
+}
+// include the SD library:
+//#include <SPI.h>
+
+
+// set up variables using the SD utility library functions:
+//Sd2Card card;
+
