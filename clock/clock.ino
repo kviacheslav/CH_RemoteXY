522010fe676b9811a07740ce5e143f54ae7788a5
kviacheslav <k.viacheslav@rambler.ru>
2020-11-22 09:11:18 +0300

    Из diagrams.net 13.9.9

--- clock/clock.ino
+++ clock/clock.ino
@@ -37,22 +37,34 @@
 // конфигурация интерфейса  
 #pragma pack(push, 1)
 uint8_t RemoteXY_CONF[] =
-   { 255,5,0,45,0,67,0,10,13,1,
-  66,132,19,16,29,23,2,24,2,0,
-  21,56,24,13,2,26,31,31,79,78,
-  0,79,70,70,0,7,44,21,44,23,
-  7,2,26,2,2,65,36,6,22,13,
-  13,66,0,49,18,9,21,2,26,67,
-  5,3,92,57,4,2,26,41,65,34,
-  31,28,5,5 };
-  
+   { 255,8,0,64,0,179,0,10,13,5,
+  66,132,19,16,29,23,1,2,24,2,
+  0,21,56,24,13,1,2,26,31,31,
+  79,78,0,79,70,70,0,7,44,21,
+  44,23,7,1,2,26,2,2,65,36,
+  6,22,13,13,1,66,0,49,18,9,
+  21,1,2,26,67,5,3,92,57,4,
+  0,2,26,41,65,34,31,28,5,5,
+  1,131,1,0,0,20,7,1,2,24,
+  82,101,108,97,121,0,131,0,43,0,
+  20,7,2,2,24,67,108,111,99,107,
+  0,67,1,13,10,16,16,2,2,26,
+  3,67,1,3,30,56,5,2,2,26,
+  11,1,8,26,43,12,12,2,2,31,
+  115,101,116,0,1,2,10,47,6,6,
+  2,2,31,45,0,1,2,46,47,6,
+  6,2,2,31,43,0,67,1,35,10,
+  16,16,2,2,26,3,67,1,29,13,
+  6,11,2,2,26,2 };
 // структура определяет все переменные и события вашего интерфейса управления 
 struct {
 
-   
-    // input variables
+      // input variables
   uint8_t switch_1; // =1 если переключатель включен и =0 если отключен 
   float T_min;
+  uint8_t button_set; // =1 если кнопка нажата, иначе =0 
+  uint8_t button_decrement; // =1 если кнопка нажата, иначе =0 
+  uint8_t button_increment; // =1 если кнопка нажата, иначе =0 
 
     // output variables
   int8_t level_T; // =0..100 положение уровня 
@@ -60,7 +72,12 @@ struct {
   int8_t level_H; // =0..100 положение уровня 
   char text_1[41];  // =строка UTF8 оканчивающаяся нулем 
   uint8_t led_2_g; // =0..255 яркость зеленого цвета индикатора 
+  char TimeH[3];  // =строка UTF8 оканчивающаяся нулем 
+  char Date[11];  // =строка UTF8 оканчивающаяся нулем 
+  char TimeM[3];  // =строка UTF8 оканчивающаяся нулем 
+  char TimeP[2];  // =строка UTF8 оканчивающаяся нулем 
 
+  
     // other variable
   uint8_t connect_flag;  // =1 if wire connected, else =0 
 
@@ -96,14 +113,21 @@ float hum;
 float set_tem;
 int tem_last;
 int hum_last;
+int change; 
 uint8_t power_on;
 uint8_t display_show = 1; // display time and temperatur
+uint8_t set_show = 0; // set time and date
 boolean relay = false;    // relay off, true - on
+boolean set = true;     // set blink, false - pause
+boolean set_press = false;     // press , false - unpress
+boolean increment_press = false;     // press , false - unpress
+boolean decrement_press = false;     // press , false - unpress
 boolean light = true;     // diod lighting, false - pause
 boolean button = false;   // true - button press
 uint32_t dhtTimeOut;
 uint32_t rtcTimeOut;
 uint32_t displayTimeOut;
+uint32_t setTimeOut;
 
 void setup() 
 {
@@ -111,7 +135,7 @@ void setup()
   RemoteXY_Init ();   
   
   // TODO you setup code
-  rtcTimeOut = dhtTimeOut = displayTimeOut = millis();
+  setTimeOut = rtcTimeOut = dhtTimeOut = displayTimeOut = millis();
   
   //--------PIN SETUP ------------
   pinMode(PIN_BUTTON, INPUT);
@@ -135,8 +159,8 @@ void setup()
       tem_last = 0;
       RemoteXY.led_2_g = 0;
   } else {
-      hum_last = round(hum);
-      tem_last = round(tem);
+      hum_last = hum;
+      tem_last = tem;
       RemoteXY.led_2_g = 128;
   }
   RemoteXY.level_H = hum_last;
@@ -237,19 +261,27 @@ void loop()
     hum = dht.readHumidity();    
     tem = dht.readTemperature();
     if (isnan(hum) && isnan(tem)) {
-        REMOTEXY__DEBUGLOGS.println("DHT failed");
+        REMOTEXY__DEBUGLOGS.println("DHT failed"); 
+        RemoteXY.led_2_g = 0;       
     } else {
         REMOTEXY__DEBUGLOGS.print("Temperature: ");
         REMOTEXY__DEBUGLOGS.print(tem,1);
         REMOTEXY__DEBUGLOGS.print(" Humidity: ");
-        REMOTEXY__DEBUGLOGS.println(hum,1);        
+        REMOTEXY__DEBUGLOGS.println(hum,1);
+        hum_last = hum;
+        tem_last = tem;
+        RemoteXY.led_2_g = 128;        
     }
-    RtcTemperature temp = Rtc.GetTemperature();
-   
-    temp.Print(Serial);
+    relay = power(RemoteXY.T_min - tem_last);
+    RtcTemperature temp = Rtc.GetTemperature();   
+    temp.Print(REMOTEXY__DEBUGLOGS);
     // you may also get the temperature as a float and print it
     // Serial.print(temp.AsFloatDegC());
     REMOTEXY__DEBUGLOGS.println("C");
+    
+    RemoteXY.level_H = hum_last;
+    RemoteXY.level_T = tem_last + 50;
+       
     dhtTimeOut = millis();
   }
   
@@ -281,7 +313,7 @@ void loop()
       
       rtcTimeOut = millis();
     }
-
+    
     if (digitalRead(PIN_BUTTON)) {
       if (!button) //подождем пока отпустишь
         button = true;
@@ -295,6 +327,48 @@ void loop()
         displayTimeTemp(Rtc.GetDateTime(),tem,hum);
         button = false;
       }         
+    } 
+    if (RemoteXY.button_set){
+          if (!set_press){
+            if (set_show == 0)
+              set_show = 1;
+            else 
+              if (++set_show > 5)
+                set_show = 0;
+            set_press = true;                             
+          }                    
+    } else {
+          if (set_press)
+            set_press = !set_press;                       
+    }  
+    if (RemoteXY.button_increment){
+          if (!increment_press){
+            change = 1;
+            increment_press = true;                             
+          }                    
+    } else {
+          if (increment_press){
+            increment_press = false; 
+            change = 0;
+          }                       
+    } 
+    if (RemoteXY.button_decrement){
+          if (!decrement_press){
+            change = -1;
+            decrement_press = true;                             
+          }                    
+    } else {
+          if (decrement_press){
+            decrement_press = false; 
+            change = 0;
+          }                       
+    }   
+
+
+    if ((millis() - setTimeOut) > 1000) {        
+      setTimeDate(Rtc.GetDateTime(), set_show, set, change); 
+      set = !set;
+      setTimeOut = millis();
     }
     
     
@@ -302,8 +376,89 @@ void loop()
 
 #define countof(a) (sizeof(a) / sizeof(a[0]))
 
+void setTimeDate(const RtcDateTime& dt, uint8_t set_show, boolean set, int change){
+  
+  uint16_t year = dt.Year();
+  uint8_t hour = dt.Hour();
+  uint8_t minute = dt.Minute();
+  uint8_t second = dt.Second();
+  uint8_t day = dt.Day();
+  uint8_t month = dt.Month();
+
+  snprintf_P(RemoteXY.TimeH, 
+    countof(RemoteXY.TimeH),
+    PSTR("%02u"),
+    hour
+  );
+   snprintf_P(RemoteXY.TimeM, 
+    countof(RemoteXY.TimeM),
+    PSTR("%02u"),
+    minute
+  );
+  RemoteXY.TimeP[0] = ':';
+  snprintf_P(RemoteXY.Date, 
+    countof(RemoteXY.Date),
+    PSTR("%02u.%02u.%04u"),
+    day,
+    month,            
+    year
+  ); 
+  switch (set_show) {
+          case 1: 
+            if(change > 0){
+              if (++hour > 23)
+                hour = 0;
+            } else {
+              if (hour == 0)
+                hour = 59;
+              else
+               --hour;                
+            }
+            if (set)            
+              RemoteXY.TimeH[0] = RemoteXY.TimeH[1] = '_';            
+            break;
+          case 2:
+            if(change > 0){
+              if (++minute > 59)
+                minute = 0;
+            } else {
+              if (minute == 0)
+                minute = 59;
+              else
+               --minute;                
+            }
+            if (set)
+              RemoteXY.TimeM[0] = RemoteXY.TimeM[1] = '_';  
+            break; 
+          case 3: 
+           if (set)
+              RemoteXY.Date[0] = RemoteXY.Date[1] = '_';  
+           break; 
+          case 4:
+            if (set)
+              RemoteXY.Date[3] = RemoteXY.Date[4] = '_';
+            break; 
+          case 5:
+            if (set)
+              RemoteXY.Date[6] = RemoteXY.Date[7] = RemoteXY.Date[8] = RemoteXY.Date[9] ='_'; 
+            break;      
+          default:
+           if (set)            
+              RemoteXY.TimeP[0] = ' '; 
+           break; 
+  } 
+ 
+
+  if (change != 0){
+    RtcDateTime set_time = RtcDateTime(year, month, day, hour, minute, second);
+    printDateTime(set_time);
+    Rtc.SetDateTime(set_time);
+  }                    
+}
+
 void displayTimeTemp(const RtcDateTime& dt,float tem,float hum){
   char datestring[7];
+  
   display.clearDisplay();
   if (display_show){
     snprintf_P(datestring, 
